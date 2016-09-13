460f605d7b77e26399a5ee28cad0330ae477407e
Anthony Enem <anthony@Anthonys-MacBook-Air.local>
2016-09-12 21:54:48 -0500

    adding homework 1

--- assignments/homework-01/myrandom.sh
+++ assignments/homework-01/myrandom.sh
@@ -5,21 +5,11 @@ array=($(cat /usr/share/dict/words))
 
 #get length of array 
 let length=${#array[@]}
-<<<<<<< HEAD
-echo $length
-length=$(($length-1))
-echo $length
-=======
 #decrement length
 length=$(($length-1))
->>>>>>> bfec6cde0e0a98d2f04d7fb49a40455aa2e38d14
 
 #get random number between 0 and length inclusive
 let random=$(shuf -i 0-$length -n 1)
 
-<<<<<<< HEAD
-echo ${array[$random]}
-=======
 #print word at random index in array
 echo ${array[$random]}
->>>>>>> bfec6cde0e0a98d2f04d7fb49a40455aa2e38d14
