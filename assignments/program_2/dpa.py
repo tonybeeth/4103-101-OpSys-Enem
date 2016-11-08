4feb22ff8559f25191662377310da00f4a147ecc
Anthony Enem <Anthony Enem>
2016-11-07 23:19:41 -0600

    program 2

--- assignments/program_2/dpa.py
+++ assignments/program_2/dpa.py
@@ -1,101 +1,12 @@
+#Anthony Enem
+#Program 2
+
 import threading
-import os
-from os import system
-import curses
-import locale
 import time
-import threading
+from threading import Lock
+from time import sleep
+import os
 import random
-import json
-import struct
-
-screenLock = threading.Lock()
-
-"""
-Location object to help the Curses Window class
-"""
-class Cell(object):
-    def __init__(self,row=0,col=0):
-        self.row = row
-        self.col = col
-
-"""
-Curses Window wrapper to help with printing to the screen
-"""
-class CursesWindow(object):
-        
-    def __init__(self):
-        self.screen = curses.initscr()
-
-        self.maxy,self.maxx = self.screen.getmaxyx()
-        curses.start_color()
-        curses.use_default_colors()
-        curses.cbreak()
-        self.colors = self.loadColors()
-
-        for c in self.colors:
-            r,g,b = c['curses']
-            i = int(c['index'])
-            curses.init_color(i,r,g,b)
-            curses.init_pair(i,i,-1)
-
-        self.screen.border(0)    
-
-    """
-    Loads the possible colors from colors.csv
-    """
-    def loadColors(self):
-        f = open("colors.csv","r")
-
-        colors = []
-
-        for line in f:
-            temp = line.strip()
-            temp = temp.split(',')
-            rgb = struct.unpack('BBB',temp[2].strip('#').decode('hex'))
-            curses = (int(rgb[0]/255.0*1000.0),int(rgb[1]/255.0*1000.0),int(rgb[2]/255.0*1000.0))
-            colors.append({'index':temp[0],'name':temp[1].strip(),'hex':temp[2].strip(),'curses':curses,'rgb':rgb})
-
-        f.close()
-        
-        return colors
-    
-    def getColorNames(self):
-        names = []
-        for c in self.colors:
-            names.append(c['name'])
-        return names
-
-    
-    def cprint(self,row,col,string,color=0):
-        try: 
-
-            self.screen.addstr(row, col, string, curses.color_pair(color))
-            self.screen.addch(row, col-2, '#',curses.color_pair(color))      
-        except:
-            print("Unexpected error:", row, col, string, color)
-            
-            raise
-        self.screen.refresh()
-    
-    def randomColor(self):
-        """visibile colors"""
-        return random.randint(1,len(self.colors))
-        
-    def getColor(self,key,val):
-        
-        for c in self.colors:
-            if c[key] == val:
-                return c
-        
-        return None
-        
-    def exit(self):
-        self.screen.getch()
-        curses.endwin()
-        
-
-"""=========================================================="""
 
 # Layout of the table (P = philosopher, f = fork):
 #          P0
@@ -113,72 +24,63 @@ numPhilosophers = 4
 philosophers = []
 forks = []
 
-screenLock = threading.Lock()
+class Waiter:
+    def __init__(self):
+        self.lock = Lock()
+        self.lockingThread = -1        
+
+    def getForks(self, phil_num):
+
+        self.lock.acquire() #acquire waiter lock
+        self.lockingThread = phil_num
+
+        #assign left and right forks for Philosopher
+        leftForkIndex = phil_num
+        rightForkIndex = (phil_num - 1) % numPhilosophers
+
+        while True: #while current Philosopher has not eaten
+
+            if forks[rightForkIndex].acquire(False): #try acquiring right fork
+                if forks[leftForkIndex].acquire(False): #try acquiring left fork
+
+                    self.lock.release() #release waiter lock after philosopher acquires both forks
+                    
+                    startTime = time.time()
+                    sleep(0.01) #hold forks for 0.01 seconds
+                    print "Philosopher", phil_num, "ate for", time.time() - startTime, "seconds."
+
+                    #release forks
+                    forks[rightForkIndex].release()
+                    forks[leftForkIndex].release()
+
+                    break #break out of while loop after eating
+
+                else:
+                    forks[rightForkIndex].release() #release right fork acquired
+
+
 
 class Philosopher(threading.Thread):
-    def __init__(self, index,window,cell):
+    def __init__(self, index, waiter):
         threading.Thread.__init__(self)
         self.index = index
-        self.window = window
-        self.cell = cell
-        self.color = self.window.randomColor()  # Color to draw with
-        
+        self.waiter = waiter
 
     def run(self):
-        # Assign left and right fork
-        leftForkIndex = self.index
-        rightForkIndex = (self.index - 1) % numPhilosophers
-        forkPair = ForkPair(leftForkIndex, rightForkIndex)
-        
-        # This prints out the threads name on the left of our "progress bar"
-        with screenLock:
-            self.window.cprint(self.cell.row, self.cell.col, str(self.index),self.color)
-        self.cell.col += 5
-
-        # Eat forever
+        # Run forever
         while True:
-            forkPair.pickUp()
-            with screenLock:
-                self.window.cprint(self.cell.row, self.cell.col, "#" ,self.color)
-                self.cell.col += 1
-                if self.cell.col >= self.window.maxx-2:
-                    self.cell.col = 10
-                    for i in range(10,self.window.maxx-2):
-                        self.window.cprint(self.cell.row, i, "#",16)
-                time.sleep(.05)
-            time.sleep(.01)
-            forkPair.putDown()
+            self.waiter.getForks(self.index)
 
-class ForkPair:
-    def __init__(self, leftForkIndex, rightForkIndex):
-        # Order forks by index to prevent deadlock
-        if leftForkIndex > rightForkIndex:
-            leftForkIndex, rightForkIndex = rightForkIndex, leftForkIndex
-        self.firstFork = forks[leftForkIndex]
-        self.secondFork = forks[rightForkIndex]
-    
-
-    def pickUp(self):
-        # Acquire by starting with the lower index
-        self.firstFork.acquire()
-        self.secondFork.acquire()
-
-    def putDown(self):
-        # The order does not matter here
-        self.firstFork.release()
-        self.secondFork.release()
 
 if __name__ == "__main__":
+    waiter = Waiter()
 
-    screenLock = threading.Lock()
-    window = CursesWindow()
-    row = 5
-    
-    # Create philosophers and forks
+     # Create philosophers and forks
     for i in range(0, numPhilosophers):
-        philosophers.append(Philosopher(i,window,Cell(5+row,5)))
+        philosophers.append(Philosopher(i, waiter))
         forks.append(threading.Lock())
-        row += 1
+    
+    #random.shuffle(philosophers) #for more unpredictable results 
 
     # All philosophers start eating
     for philosopher in philosophers:
@@ -186,6 +88,6 @@ if __name__ == "__main__":
 
     # Allow CTRL + C to exit the program
     try:
-        while True: time.sleep(021)
+        while True: sleep(0.1)
     except (KeyboardInterrupt, SystemExit):
         os._exit(0)
